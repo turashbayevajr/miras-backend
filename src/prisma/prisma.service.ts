85e411fe4ec79878e8c1b1b3f0b42a7087f7fe2a
turashbayevajr <103361172+turashbayevajr@users.noreply.github.com>
2025-05-15 20:43:56 +0500

    prisma fix #1

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,18 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    await this.$connect();
+    let retries = 5;
+    while (retries) {
+      try {
+        await this.$connect(); // ✅ ← исправлено
+        console.log('✅ Connected to DB');
+        break;
+      } catch (err) {
+        console.log('⏳ DB not ready, retrying...', err.message);
+        retries -= 1;
+        await new Promise(res => setTimeout(res, 5000));
+      }
+    }
   }
 
   async onModuleDestroy() {
